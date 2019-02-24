2a2c84602f780786f06878ac54f1e765c2568239
Leonardo Feitosa <leonardofeitosa3010@gmail.com>
2019-02-24 15:59:47 -0300

    Adding DashboardComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,21 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule} from '@angular/router';
 
-const ROUTES = RouterModule.forRoot ([
-  { path: 'tasks', component: TasksComponent }
-]);
-
-
 import { AppComponent } from './app.component';
+import { TaskDetailComponent } from './tasks/task-detail/task-detail.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TasksComponent } from './tasks/tasks.component';
-import { TaskDetailComponent } from './tasks/task-detail/task-detail.component';
 
+const ROUTES = RouterModule.forRoot ([
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'tasks', component: TasksComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+]);
 @NgModule({
   declarations: [
     AppComponent,
+    DashboardComponent,
     NavbarComponent,
     TasksComponent,
     TaskDetailComponent

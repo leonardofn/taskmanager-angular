f1056df5e131e3cdb63022360f26def63794e4a5
Leonardo Feitosa <leonardofeitosa3010@gmail.com>
2019-02-27 09:31:17 -0300

    Installing and adding http to TaskService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 // angular imports
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { NgModule } from '@angular/core';
+
 
 // components imports
 import { AppComponent } from './app.component';
@@ -27,7 +29,8 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule
   ],
   providers: [ TaskService ],
   bootstrap: [AppComponent]
--- src/app/tasks/shared/task-service.ts
+++ src/app/tasks/shared/task-service.ts
@@ -1,3 +1,4 @@
+import { HttpModule } from '@angular/http';
 import { Injectable } from '@angular/core';
 
 import { Task } from './task.model';
@@ -15,6 +16,7 @@ const TASKS: Array<Task> = [
 @Injectable()
 
 export class TaskService{
+    public constructor(private http: HttpModule){ }
 
     public getTasks(): Promise<Task[]>{ // public getTasks(): Promise<Task[]>{}
         // Deve-se especificar o tipo Promise de retorno diretamente: return new Promise((resolve, reject) => {}

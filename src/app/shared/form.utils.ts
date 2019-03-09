df7f514275fbe72d0540dcfb1f1bc087c8856076
Leonardo Feitosa <leonardofeitosa3010@gmail.com>
2019-03-09 20:38:57 -0300

    Adding FormUtils class

---
+++ src/app/shared/form.utils.ts
@@ -0,0 +1,23 @@
+import { FormGroup } from '@angular/forms';
+
+export class FormsUtils{
+    public constructor(private form: FormGroup){
+
+    }
+
+    public fieldClassForErrorOrSuccess(fieldName: string){
+        return{
+            "is-invalid": this.showFieldError(fieldName),
+            "is-valid": this.getField(fieldName).valid
+        }
+    }
+
+    public showFieldError(fieldName: string): boolean{
+        let field = this.getField(fieldName);
+        return field.invalid && (field.touched || field.dirty);
+    }
+
+    public getField(fieldName: string){
+        return this.form.get(fieldName);
+    }
+}

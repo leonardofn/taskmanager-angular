19d1e181535c868fcd96ad21b1b7e7e4a1223673
Leonardo Feitosa <leonardofeitosa3010@gmail.com>
2019-03-23 13:38:32 -0300

    Handling backEnd SignUp errors

--- src/app/sign-up-form/sign-up-form.component.ts
+++ src/app/sign-up-form/sign-up-form.component.ts
@@ -15,23 +15,42 @@ import { User } from '../shared/user.model';
 export class SignUpFormComponent {
     public form: FormGroup;
     public formUtils: FormUtils;
+    public submitted: boolean;
+    public formErrors: Array<string>;
 
-    public constructor(private authService: AuthService, private formBuilder: FormBuilder, private router: Router){
+    public constructor(private authService: AuthService, private formBuilder: FormBuilder, private router: Router) {
         this.setupForm();
         this.formUtils = new FormUtils(this.form);
+        this.submitted = false;
+        this.formErrors = null;
     }
 
-    public signUpUser(){
+    public signUpUser() {
+        this.submitted = true;
+
         this.authService.signUp(this.form.value as User)
             .subscribe(
+                // Sucesso
                 () => {
                     alert("Parabéns, sua conta foi criada com sucesso!");
                     this.router.navigate(['/dashboard']);
+                    this.formErrors = null;
+                },
+                // Error
+                (error) => {
+                    this.submitted = false;
+
+                    if (error.status === 422) {
+                        // Captura mensagem de erro visível no console do browser
+                        this.formErrors = error.error.errors.full_messages;
+                    }else {
+                        this.formErrors = ["Não foi possível processar sua solicitação. Por favor, tente mais tarde."];
+                    }
                 }
-            )
+            );
     }
 
-    public passwordConfirmationValidator(form: FormGroup){
+    public passwordConfirmationValidator(form: FormGroup) {
         if (form.get('password').dirty && form.get('password').value === form.get('passwordConfirmation').value) {
             form.get('passwordConfirmation').setErrors(null);
         }else{
@@ -39,7 +58,7 @@ export class SignUpFormComponent {
         }
     }
 
-    private setupForm(){
+    private setupForm() {
         this.form = this.formBuilder.group({
             name: [null, [Validators.required, Validators.minLength(5), Validators.maxLength(100)]],
             login: [null, [Validators.required, Validators.email]],
